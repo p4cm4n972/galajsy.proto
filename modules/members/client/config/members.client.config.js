d323ee6b9181401de16aa293411015649f2e1711
p4cm4n972 <manuel.adele@gmail.com>
2017-07-26 17:28:00 +0200

    ..... members

---
+++ modules/members/client/config/members.client.config.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('members')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Members',
+      state: 'members.list',
+      type: 'item',
+      roles: ['*']
+    });
+  }
+}());
