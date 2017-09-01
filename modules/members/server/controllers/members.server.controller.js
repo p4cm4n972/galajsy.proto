dd8efb6446c9c4f929bdfa6f41f9052ace76ddd5
p4cm4n972 <manuel.adele@gmail.com>
2017-09-01 16:55:27 +0200

    Ajouter un utilisateur à la liste d’amis: envoie mail

--- modules/members/server/controllers/members.server.controller.js
+++ modules/members/server/controllers/members.server.controller.js
@@ -55,8 +55,8 @@ exports.favorite = function (req, res) {
         }
       });
       let mailOptions = {
-        from: req.user.email || req.user.email,
-        to: req.member.providerData.email || req.member.email,
+        from: req.member.email || req.member.email,
+        to: req.user.providerData.email || req.user.email,
         subject: 'Be my friend | galaJSy 🤖 ',
         html: '<h1 style="border: solid green 2px; text-align:center"><i>Vous avez reçu une demande de: ' + req.member.username + '</i></h1><img src=' + req.member.profileImageURL || req.member.providerData.profileImageURL + ' width=150px>'
       };
